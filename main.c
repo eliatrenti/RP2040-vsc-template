f303136eba101f693309c5dc106eec68fb393418
elia_trenti20 <elia.trenti@gmail.com>
2022-05-16 11:41:27 +0200

    Aggiunta della seriale over USB

--- main.c
+++ main.c
@@ -1,15 +1,16 @@
- #include "pico/stdlib.h"
  #include <stdio.h>
+ #include "pico/stdlib.h"
 
  int main() {
      const uint led_pin = 25; 
      int i = 0;
      gpio_init(led_pin);
      gpio_set_dir(led_pin,GPIO_OUT);
+     stdio_init_all();
      printf("Hey, sono partito!\r\n");
 
      while (true){
-         
+
          gpio_put(led_pin,false);
          sleep_ms(100);
          gpio_put(led_pin,true);
@@ -18,7 +19,7 @@
          sleep_ms(100);
          gpio_put(led_pin,true);
          sleep_ms(1000);
-         printf(i++ + "\n");
-         
+        printf("Heartbeat number: %d \n", i++);
+        sleep_ms(1000);
      }
  }
